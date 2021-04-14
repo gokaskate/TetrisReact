8efa3ce58c9086aa6e169bbfc0d9f9c1602dd6ec
Santiago Beltran <gokaskate@gmail.com>
2021-04-13 20:40:19 -0500

    Update gameHelper.jsx

--- src/gameHelper.jsx
+++ src/gameHelper.jsx
@@ -13,7 +13,7 @@ export const checkCollision = (player, stage, { x: moveX, y: moveY }) => {
                 if (
                 !stage[y + player.pos.y + moveY] ||
                 !stage[y + player.pos.y + moveY][x + player.pos.x + moveX] ||
-                stage [y + player.pos.y + moveY][x + player.pos.x + moveX][1] !== 'clear'
+                stage[y + player.pos.y + moveY][x + player.pos.x + moveX][1] !== 'clear'
                 ) {
                     return true;
                 }
